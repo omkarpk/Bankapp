441c3ce632980f8c6558d811e1757e564bbfc832
Neha Bulakhe <nehabulakhe0922@gmail.com>
2023-12-15 15:28:46 +0530

    create Component folder

--- App.tsx
+++ App.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
 function App(): React.JSX.Element {
   return (
     <View>
-      <Text>Banking app</Text>
-      <Text>Banking app</Text>
-      <Text>Banking app</Text>
       <Text>Banking app</Text>
     </View>
   );
